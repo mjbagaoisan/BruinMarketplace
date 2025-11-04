0fc499d92e80f2b70e75aac4e62c78180c384eeb
Marc Jowell Bagaoisan <marcjowellgbagaoisan@gmail.com>
2025-11-03 17:05:59 -0800

    Added login page and middleware

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,45 @@
+import { buttonVariants } from "@/components/button";
+import AuthButton from "@/components/AuthButton";
+import { cn } from "@/lib/utils";
+import { ChevronLeft } from "lucide-react";
+import { Metadata } from "next";
+import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: "Login | BruinMarketplace",
+  description: "Login to your account",
+};
+
+export default function LoginPage() {
+  return (
+    <div className="min-h-screen w-full flex items-center justify-center p-4">
+      <div className="fixed top-4 left-4 md:left-8 md:top-8">
+        <Link
+          href="/"
+          className={cn(
+            buttonVariants({ variant: "ghost" }),
+            "flex items-center"
+          )}
+        >
+          <ChevronLeft className="mr-2 h-4 w-4" />
+          Back
+        </Link>
+      </div>
+      <div className="w-full max-w-[400px] flex flex-col gap-8 p-8 bg-card rounded-lg shadow-lg">
+        <div className="flex flex-col gap-4 text-center">
+          {/* <Icons.logo className="mx-auto h-10 w-10" /> */}
+          <h1 className="text-3xl font-bold tracking-tight">
+            Welcome! 
+          </h1>
+          <p className="text-base text-muted-foreground">Login/Create your account</p>
+        </div>
+        <div className="w-full">
+          <AuthButton />
+        </div>
+        <p className="text-center text-sm text-muted-foreground">
+          By continuing, you agree to our Terms of Service and Privacy Policy
+        </p>
+      </div>
+    </div>
+  );
+}
